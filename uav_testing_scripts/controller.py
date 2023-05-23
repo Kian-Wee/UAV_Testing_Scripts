16d2d6000eec6128d795b0ce0886603956ae1cd9
Kian Wee <kianweetan2002@gmail.com>
2023-05-23 16:28:52 +0800

    added controller

--- uav_testing_scripts/controller.py
+++ uav_testing_scripts/controller.py
@@ -17,15 +17,19 @@ from rclpy.node import Node
 
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import String
+import numpy as np
 
 # For alignment of camera_frame to drone_frame(CG)
 bodywidth_x =0
 bodywidth_y =0
 bodywidth_z =0
 
+# Camera Topic for desired setpoint
+camera_setpoint_topic="/camera_setpoint"
+
 # Setpoint Topic to publish to
-setpoint_topic="/uav1/mavros/setpoint_position/local"
-local_position_topic="/uav1/mavros/local_position/pose"
+setpoint_topic="/mavros/setpoint_position/local"
+local_position_topic="/mavros/local_position/pose"
 
 # Update rate
 rate = 1/10 #10 times every second
@@ -38,21 +42,33 @@ class AlignmentController(Node):
         self.camera_setpoint = PoseStamped()
         self.subscription = self.create_subscription(
             PoseStamped,
-            'topic',
+            camera_setpoint_topic,
             self.camera_listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
-        self.local_setpoint = PoseStamped()
-        self.local_setpoint_subscription = self.create_subscription(
+        self.local_position = PoseStamped()
+        self.local_position_subscription = self.create_subscription(
             PoseStamped,
             local_position_topic,
-            self.camera_listener_callback,
+            self.local_position_listener_callback,
             10)
-        self.local_setpoint_subscription  # prevent unused variable warning
+        self.local_position_subscription  # prevent unused variable warning
+        self.eul_deg=np.array([0,0,0])
+        self.rad_deg=np.array([0,0,0,0])
 
         self.setpoint_publisher_ = self.create_publisher(PoseStamped, setpoint_topic, 10)
-        self.rate = self.create_timer(rate, self.timer_callback)
+        # self.rate = self.create_timer(rate, self.timer_callback)
+
+        # Controller variables
+        self.kp_x = 0.5
+        self.kd_x = 0.125
+        self.kp_y = 0.25
+        self.kd_y = 0.0625
+        self.kp_z = 0.2
+        self.kd_z = 0.8
+        self.error_past = np.array([0,0,0])
+        self.feedback = np.array([self.local_position.pose.position.x, self.local_position.pose.position.y, self.local_position.pose.position.z]) # FOR REFERENCE ONLY, DELETE LATER
 
     def camera_listener_callback(self, msg):
         # self.get_logger().info('I heard: "%s"' % msg.data)
@@ -60,6 +76,9 @@ class AlignmentController(Node):
 
     def local_position_listener_callback(self, msg):
         self.local_setpoint = msg
+        # self.eul_deg=rad2deg(quat2eul(rb(i).Quaternion,"XYZ"))
+        # self.eul_rad= quat2eul(rb(i).Quaternion,"XYZ")
+        self.caculate_offset(self.camera_setpoint,self.local_position) # caculate for a new controller input everytime the UAV moves
 
     def timer_callback(self):
         msg = PoseStamped()
@@ -72,11 +91,25 @@ class AlignmentController(Node):
         msg.pose.orientation.w = 1
         self.setpoint_publisher_.publish(msg)
 
-    def caculate_offset(self, desired, current):
-        desired = self.camera_setpoint
-        current = self.local_setpoint
+    def caculate_offset(self, setpoint, current):
+        # Error = Setpoint - Feedback
+        self.error = np.subtract(np.array([setpoint.pose.position.x, setpoint.pose.position.y, setpoint.pose.position.z]),np.array([current.pose.position.x, current.pose.position.y, current.pose.position.z]) )
+        # Derivative error = Error - error_past
+        self.derivative_error = self.error - self.error_past
+
+        input_z = (self.kp_z * self.error[2] * 1) + (self.kd_z * self.derivative_error[2])
+        input_x = ((self.kp_x * self.error[0] * 1) + (self.kd_x * self.derivative_error[0]))
+        input_y = ((self.kp_y * self.error[1] * 1) + (self.kd_y * self.derivative_error[1]))
+
+        msg = PoseStamped()
+        msg.pose.position.x= input_x
+        msg.pose.position.y= input_y
+        msg.pose.position.z= input_z
+        msg.pose.orientation.w = 1
+        self.setpoint_publisher_.publish(msg)
 
 
+        self.error_past = self.error
         
 
 def main(args=None):
